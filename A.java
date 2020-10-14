2d59ad8ce0a50844ab60e30a32e421379c407ad7
Gaidon90 <gaidonis@citycollege.sheffield.eu>
2020-10-14 21:57:24 +0300

    Changes to both methods

--- A.java
+++ A.java
@@ -7,12 +7,13 @@ public class A
     public void doIt()
     {
         int x=0;
+        int y=0;
     }
 }
-
+        //Last time I promise
     public void somebodyHelpMe()
     {
         int y=0;
-        itn x=0;   
+        int x=0;   
     }
 }
